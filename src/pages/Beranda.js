14d56ae18a242f1250844b999180ec4724f696eb
tarripewe <tarripewe01@gmail.com>
2021-09-03 08:33:10 +0700

    Unit Kerja Beranda: Fix UI

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Footerr from "../components/Footer";
+import { Layout } from "antd";
+import { Content, Footer } from "antd/lib/layout/layout";
 import ListPaymentRequest from "../components/ListPaymentRequest";
 
 const Beranda = () => {
   return (
-    <div className="beranda">
-      <ListPaymentRequest />
-    </div>
+    // <div className="beranda">
+    //   <ListPaymentRequest />
+    // </div>
+    <>
+      <Layout style={{ minHeight: "100vh", backgroundColor: "#fff" }}>
+        <Content style={{ marginTop: "50px" }}>
+          <ListPaymentRequest />
+        </Content>
+        <Footerr />
+      </Layout>
+    </>
   );
 };
 
